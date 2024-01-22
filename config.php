24ec2438b36896fbb6b13fe768c165abf74ddc07
Amenu-sh <amanushifara@gmail.com>
2024-01-22 10:12:08 -0600

    first commit

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+//DB credentials
+$sql_db_host = "localhost";
+$sql_db_user = "evangadiQandA";
+$sql_db_name = "evangadiQandA";
+$sql_db_pass = "rQdjV9[kr6]y)bZH";
+
+
+
+
+
+
---
+++ helpers/connect.php
@@ -0,0 +1,4 @@
+<?php
+$root = realpath($_SERVER["DOCUMENT_ROOT"]);
+require_once($root."/config.php");
+
